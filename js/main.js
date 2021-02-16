e84f823255f0f4435f441a36d19cd2d70ec48cf3
EmillySSilva <emillysilva1@gmail.com>
2021-02-15 22:04:37 -0300

    adicionado o arquivo primeira_pagina e a pasta js

---
+++ js/main.js
@@ -0,0 +1,97 @@
+//var name="Emilly Silva";
+//var idade=29;
+//var idade2=10;
+//var frase="japão é o melhor time do mundo";
+//alert(nome+" tem "+ idade +" anos");
+//alert(idade+idade2);*/
+//console.log(name);
+//console.log(idade+idade2);
+//console.log(frase.replace("Japão"+"Brasil"));
+//alert(frase.replace("Japão","Brasil"));
+//console.log(frase.toLowerCase());
+
+//var lista=["maçã", "pêra", "laranja"];
+//lista.push("uva"); // adicionar item
+//lista.pop(); // retirar item
+//console.log(lista);
+//console.log(lista.length); // tamalho da lista
+//console.log(lista.reverse()); //trazer os valores invertidos na lista
+//console(lista);// imprimir em formato de lista
+//console.log(lista.toString()); imprimir em formato de frase
+//console.log(lista.join( - )); //imprimir em Spring tira a virgula e coloca o caracter determinado pelo join
+
+
+/*var fruta={nome:"maçã", cor:"vermelja"};
+console.log(fruta.nome);
+alerta(fruta.cor);
+*/
+
+/*var frutas =[{nome:"maçã", cor:"vermelha", nome:"uva", cor:"roxa"}];
+console.log(frutas);
+alerta(fruta[1].nome);
+*/
+/*
+var idade=prompt("Qual é a sua idade");
+// var idade
+if (idade>=18){
+   alert("maior de idade);
+} else{
+   alert("menor de idade")
+}
+*/
+
+/*
+var count=0;
+while (count<5){
+   console.log(count);
+   alert(count);
+   count++;
+}
+*/
+
+/*
+var count;
+for(count=0; count<=5; count++){
+alert(cont);
+}
+*/
+
+/*
+var d=new Date();
+//alert(d);
+alert(d.getMonth()+1);
+
+*/
+
+function soma(n1,n2){
+  return n1+n2;
+}
+function validaIdade(idade){
+   var validar;
+   if (idade>= 18){
+       validar = true
+   }else{
+       validar=false
+   }
+}
+
+var idade=prompt("Qual é sua idade?");
+console.log(validaIdade(idade));
+
+/*function setReplace(frase,name){
+   return frase.replase(nome, novo_nome)
+}
+alert(setReplace("vai japão", "japão","Brasil"));
+
+
+alert(soma(5+10));
+*/
+
+
+
+
+
+
+
+
+
